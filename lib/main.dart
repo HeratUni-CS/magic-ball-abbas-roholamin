1c27b67652b73c1102557533fa4b46e3a09ff666
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-06-08 07:55:18 +0430

    Done migic ball project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: null,
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('MAGIC BALL'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Home(),
+      ),
     ),
   );
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Random number generator
+  static int getRandom() => Random().nextInt(5) + 1;
+
+// initial state
+  int number = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () => setState(() => number = getRandom()),
+        child: Image.asset("images/ball$number.png"),
+      ),
+    );
+  }
 }
